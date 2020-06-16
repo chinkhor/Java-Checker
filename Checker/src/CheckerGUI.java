499f3751be405790acc9805e4a8b5f0b5c2d9466
chinkhor <chin.kooi.khor@gmail.com>
2020-06-16 13:21:59 +0800

    Created Checker GUI at constructor

--- Checker/src/CheckerGUI.java
+++ Checker/src/CheckerGUI.java
@@ -5,22 +5,26 @@ import javax.swing.SwingConstants;
 
 public class CheckerGUI 
 {
-
-	public static void main(String[] args) 
+	public CheckerGUI()
 	{
 		// default frame layout is BorderLayout
 		JFrame frame = new JFrame("Checker");
-		
+				
 		JLabel label = new JLabel(" Current Player: ORANGE  ", SwingConstants.RIGHT);
 		frame.add (label, BorderLayout.NORTH);
 
 		CheckerBoard board = new CheckerBoard();
 		frame.add(board, BorderLayout.CENTER);
-		
+				
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		//frame.setSize(480, 480);
 		frame.pack(); // use the size specified by the components, and pack them.
 		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) 
+	{
+		new CheckerGUI();
 	}
 
 }
